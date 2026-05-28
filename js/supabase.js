022b49d65f029251066582865b66183ce5df6633
cristiangarcia07 <cristiangarcia07@github.com>
2026-05-28 10:12:53 +0200

    switch to importmap for supabase-js ES module loading

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,4 @@
+import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
 
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
